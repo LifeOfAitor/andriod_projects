fe8e14a828f15be96dc1dec1640dfcd9c122c76c
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-18 14:00:07 +0200

    falta mejorar el aspecto visual del item_project xml

--- PortfolioOfProjects/app/src/main/java/com/example/portfolioofprojects/MainActivity.java
+++ PortfolioOfProjects/app/src/main/java/com/example/portfolioofprojects/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
+        Project[] projects = {
+                new Project("Getting Started Project", "Basic 'Hello World' app to learn how Android Studio works", R.drawable.ben_kolde_ajcipx1vdxi_unsplash),
+                new Project("BMI Calculator", "An app to calculate Body Mass Index based on user input", R.drawable.goran_ivos_t8lmin09_mo_unsplash),
+                new Project("ToDo List", "A simple task management app to organize daily activities", R.drawable.matam_jaswanth_lypq_bq97km_unsplash),
+                new Project("Weather App", "Displays current weather information using an API", R.drawable.pakata_goh_apqwhxh4tm4_unsplash),
+                new Project("Recipe Finder", "App to search and display recipes based on ingredients", R.drawable.tracy_adams_qvsm28o_k3y_unsplash),
+                new Project("Getting Started Project", "Basic 'Hello World' app to learn how Android Studio works", R.drawable.ben_kolde_ajcipx1vdxi_unsplash),
+                new Project("BMI Calculator", "An app to calculate Body Mass Index based on user input", R.drawable.goran_ivos_t8lmin09_mo_unsplash),
+                new Project("ToDo List", "A simple task management app to organize daily activities", R.drawable.matam_jaswanth_lypq_bq97km_unsplash),
+                new Project("Weather App", "Displays current weather information using an API", R.drawable.pakata_goh_apqwhxh4tm4_unsplash),
+                new Project("Recipe Finder", "App to search and display recipes based on ingredients", R.drawable.tracy_adams_qvsm28o_k3y_unsplash),
+                new Project("Getting Started Project", "Basic 'Hello World' app to learn how Android Studio works", R.drawable.ben_kolde_ajcipx1vdxi_unsplash),
+                new Project("BMI Calculator", "An app to calculate Body Mass Index based on user input", R.drawable.goran_ivos_t8lmin09_mo_unsplash),
+                new Project("ToDo List", "A simple task management app to organize daily activities", R.drawable.matam_jaswanth_lypq_bq97km_unsplash),
+                new Project("Weather App", "Displays current weather information using an API", R.drawable.pakata_goh_apqwhxh4tm4_unsplash),
+                new Project("Recipe Finder", "App to search and display recipes based on ingredients", R.drawable.tracy_adams_qvsm28o_k3y_unsplash),
+        };
+
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+        list.setAdapter(adapter);
+
     }
 }
