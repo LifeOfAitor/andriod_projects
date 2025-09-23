d6cd9988c1f23195a7f7eb6baf991544574547a2
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-23 13:55:14 +0200

    reference xml from MainActivity

--- PromoApp/app/src/main/java/com/example/promoapp/MainActivity.kt
+++ PromoApp/app/src/main/java/com/example/promoapp/MainActivity.kt
@@ -7,17 +7,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var previewButton: Button = findViewById(R.id.button_preview)
+        val previewButton: Button = findViewById(R.id.button_preview)
         //var previewButton= findViewById<Button>(R.id.button_preview)
         previewButton.setOnClickListener {
-            Toast.makeText(this, "Boton funciona", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "Boton funciona", Toast.LENGTH_LONG).show()
+            onPreviewClicked()
         }
 
+    }
+    private fun onPreviewClicked(){
+        val textEditName: TextInputEditText = findViewById(R.id.edit_text_name)
+        val textEditNumber: TextInputEditText = findViewById(R.id.edit_text_number)
+        val testString = textEditName.text.toString() + " , " + textEditNumber.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
