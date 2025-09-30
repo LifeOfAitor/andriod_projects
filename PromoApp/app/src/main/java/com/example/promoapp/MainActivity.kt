61b2caa93b4f9256e602222d7f571091715ccf36
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-30 10:48:49 +0200

    clean code on mainactivity.kt

--- PromoApp/app/src/main/java/com/example/promoapp/MainActivity.kt
+++ PromoApp/app/src/main/java/com/example/promoapp/MainActivity.kt
@@ -17,42 +17,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    // creamos las variables para cada elemento de la activity_main
-    private var contactNameEditText: TextInputEditText? = null
-    private var contactNumberEditText: TextInputEditText? = null
-    private var myDisplayNameEditText: TextInputEditText? = null
-    private var startDateEditText: TextInputEditText? = null
-    private var juniorCheckBox: CheckBox? = null
-    private var immediateStartCheckBox: CheckBox? = null
-    private var jobTitleSpinner: Spinner? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //inicializamos aqui las variables de arriba
-
-        contactNameEditText = binding.editTextName
-        contactNumberEditText = binding.editTextNumber
-        myDisplayNameEditText = binding.editTextDisplayName
-        startDateEditText = binding.editTextAvailability
-        juniorCheckBox = binding.checkBoxJunior
-        immediateStartCheckBox = binding.checkBoxJunior
-        jobTitleSpinner = binding.spinnerTitle
-
-        val previewButton: Button = binding.buttonPreview
-        previewButton.setOnClickListener {
-            //Toast.makeText(this, "Boton funciona", Toast.LENGTH_LONG).show()
+        binding.buttonPreview.setOnClickListener {
             onPreviewClicked()
         }
 
     }
-    private fun onPreviewClicked(){
-        val textEditName: TextInputEditText = binding.editTextName
-        val textEditNumber: TextInputEditText = binding.editTextNumber
-        val testString = textEditName.text.toString() + " , " + textEditNumber.text.toString()
+
+    private fun onPreviewClicked() {
+        val testString =
+            binding.editTextName.text.toString() + " , " + binding.editTextNumber.text.toString()
         Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
